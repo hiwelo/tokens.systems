cd6a6199e634c5961df4a3ce01b36386d911e0b8
Damien Senger <damien.senger@castoredc.com>
2020-05-21 16:32:40 +0200

    chore(ts): install TypeScript in this project

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@PodcastTokens`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
