9468d3f0d99f44e1cf2cf19a5c9015ffcb93c512
Damien Senger <damien.senger@castoredc.com>
2020-05-21 18:07:00 +0200

    refactor(ts): migrate pages to TypeScript and remove page2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Image from '../components/image';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
+const IndexPage: React.FC = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
