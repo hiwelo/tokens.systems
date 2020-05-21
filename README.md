ccbd379594ac72d9cba12dfa38ef9aad8b9a0efe
Damien Senger <damien.senger@castoredc.com>
2020-05-21 12:55:42 +0200

    feat(gatsby): setup new gatsby project

---
+++ README.md
@@ -0,0 +1,5 @@
+# [tokens.systems]
+
+This repository contains the sources for the website of the [tokens.systems] project.
+
+[tokens.systems]: https://tokens.systems
---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Tokens podcast`,
+    description: `tokens.systems is a podcast hosted by @iamhiwelo about the daily life of accessible and inclusive design systems projects`,
+    author: `@PodcastTokens`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `tokens-podcast`,
+        short_name: `tokens`,
+        start_url: `/`,
+        background_color: `#8b0d88`,
+        theme_color: `#8b0d88`,
+        display: `minimal-ui`,
+        icon: `src/images/tokens-systems-logo@3x.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
