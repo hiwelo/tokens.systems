91467cf6f8485d641dfe2752e2f79d7136988e4e
Damien Senger <damien.senger@castoredc.com>
2020-05-21 23:20:42 +0200

    feat(404): update content for the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,19 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
+import Chapo from '../components/Chapo';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
 const NotFoundPage: React.FC = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Oops… This page does not exist!</h1>
+    <Chapo>You just hit a route that doesn&#39;t exist... the sadness.</Chapo>
+    <Chapo>
+      If the problem persists, please{' '}
+      <Link to="https://twitter.com/PodcastTokens">contact us on Twitter!</Link>
+    </Chapo>
   </Layout>
 );
 
