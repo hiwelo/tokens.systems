9b2dfc98b0e7d20c44c4458563bb85cfdeca4aab
Damien Senger <damien.senger@castoredc.com>
2020-05-22 15:41:03 +0200

    feat(episodes): add episodes folder in filesystem & install md formatter

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,10 +11,18 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `episodes`,
+        path: `${__dirname}/src/episodes`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
